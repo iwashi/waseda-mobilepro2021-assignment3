18e1c16c6a4a583307e6cdb33c85fab2bf3996be
iwashi <iwase.yoshimasa@gmail.com>
2021-12-10 10:13:24 +0900

    first commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+class Student {
+  // ここに必要なコードを書いてください
+}
+
+void main() {
+  var student1 = Student(1);
+  student1.name = "Alice";
+  print(student1.show());
+  
+  var student2 = Student(2);
+  student2.name = "Bob";
+  print(student2.show());  
+}
